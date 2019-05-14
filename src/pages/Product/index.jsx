b0a9921f23ac1e9a920b3674ebd081f351faf60e
Usuário Convidado <Guest@PA06MICRO19.local>
2019-05-13 22:44:16 -0300

    Added product and search

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,15 +1,65 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import axios from 'axios'
+
+import './style.css'
 
 class Product extends Component {
     constructor(props) {
-        super(props);
+        super(props)
 
-        console.log(props.match.params.id);
+        this.state = {
+            id: props.match.params.id,
+            loading: true,
+            data: {},
+        }
+    }
+
+    componentDidMount() {
+        axios.all([
+            axios.get(`https://api.mercadolibre.com/items/${this.state.id}`),
+            axios.get(`https://api.mercadolibre.com/items/${this.state.id}/description`)
+        ])
+            .then(([item, description]) => {
+                this.setState({
+                    data: {
+                        ...item.data,
+                        description: description.data.plain_text,
+                    },
+                    loading: false,
+                })
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+
+    renderContent() {
+        const { data } = this.state;
+        console.log(data)
+        return (
+            <Fragment>
+                <div className="mdl-grid">
+                    <div className="mdl-cell mdl-cell--6-col">
+                        <img src={data.pictures[0].url} />
+                    </div>
+                    <div className="mdl-cell mdl-cell--6-col">
+                        <h2 className="title">{data.title}</h2>
+                        <h4 className="price">Preço: {data.base_price} {data.currency_id}</h4>
+                        <h4 className="qtd">Quantidade: {data.available_quantity}</h4>
+                        <button className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                            Comprar
+                        </button>
+                    </div>
+                    <p>{data.description}</p>
+                </div>
+            </Fragment>
+        )
     }
     render() {
-        return (
-            <div>Product</div>
-        );
+        const { loading } = this.state;
+        return loading ?
+            <div>Loading...</div> :
+            this.renderContent()
     }
 }
 
--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,9 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { Link } from 'react-router-dom'
+
+import axios from 'axios'
 
 class Search extends Component {
+    constructor() {
+        super()
+
+        this.onSearch = this.onSearch.bind(this)
+
+        this.state = {
+            results: [],
+        }
+    }
+
+    onSearch(event) {
+        const value = event.currentTarget.value
+        axios.get(`https://api.mercadolibre.com/sites/MLB/search?q=${value}`)
+            .then(({ data }) => {
+                this.setState({
+                    results: data.results,
+                })
+            })
+    }
+
+    renderItem(item) {
+        return (
+            <li key={item.id}>
+                <span>{item.id}</span>
+                <span>{item.title}</span>
+                <Link
+                    to={`product/${item.id}`}>
+                    Mostrar produto
+                </Link>
+            </li>
+        )
+    }
+
     render() {
         return (
-            <div>Search</div>
+            <Fragment>
+                <input type="text" onChange={this.onSearch} />
+
+                <ul>
+                    {this.state.results.map(this.renderItem)}
+                </ul>
+            </Fragment>
         );
     }
 }
