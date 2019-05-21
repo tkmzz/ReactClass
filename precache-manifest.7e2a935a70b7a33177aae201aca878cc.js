2eb27657fcaa71913b6b2f168dbc383a69e7189a
Luiz Monteiro <37845335+tkmzz@users.noreply.github.com>
2019-05-20 22:46:20 -0300

    Add files via upload

---
+++ precache-manifest.7e2a935a70b7a33177aae201aca878cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc58f1069bb9c7bf8140c9aae86b9a83",
+    "url": "/ReactClass/index.html"
+  },
+  {
+    "revision": "d85c9e707e26d1b55047",
+    "url": "/ReactClass/static/css/main.d54c1f2b.chunk.css"
+  },
+  {
+    "revision": "02cb9119ce965120872b",
+    "url": "/ReactClass/static/js/2.de635185.chunk.js"
+  },
+  {
+    "revision": "d85c9e707e26d1b55047",
+    "url": "/ReactClass/static/js/main.1c06f1b1.chunk.js"
+  },
+  {
+    "revision": "5f6dd6549b0220b4cb8e",
+    "url": "/ReactClass/static/js/runtime~main.49dd8631.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactClass/static/media/logo.5d5d9eef.svg"
+  }
+]);
